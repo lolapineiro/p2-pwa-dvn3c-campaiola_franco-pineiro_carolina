48f44d9e37ce26ea13eaecd179601c055f034f5c
Franco Campaiola <francocampaiola@hotmail.com>
2023-06-22 20:54:15 -0300

    feat: add views

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 let favoritas = []
 const btnObtenerDatos = document.getElementById('btndatos');
-
 const btnFavoritas = document.getElementById('btnfavorita');
+const resultado = document.getElementById('resultado');
 document.getElementById("btnfavorita").style.display = 'none';
 
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
@@ -14,7 +14,6 @@ fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
       const option = document.createElement('option');
       option.value = coin.id;
       option.text = coin.name;
-
       selectElement.appendChild(option);
     });
   })
@@ -22,24 +21,37 @@ fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
     console.log(error);
   });
 
-
-
-
 btnObtenerDatos.addEventListener('click', function () {
   const selectElement = document.getElementById('monedas');
   const selectedCoinId = selectElement.value;
   const apiUrl = `https://api.coingecko.com/api/v3/coins/${selectedCoinId}`;
+  const nombreElement = document.getElementById('nombre');
   const valorMonedaElement = document.getElementById('valor');
-  const descripcionElement = document.getElementById('descripcion');
+  const valorMaximoElement = document.getElementById('valorMaximo');
+  const valorMinimoElement = document.getElementById('valorMinimo');
+  const cambio24hElement = document.getElementById('cambio24h');
+  const ultimaActualizacionElement = document.getElementById('ultimaActualizacion');
+  
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
+        const nombre = data.name;
         const valorMoneda = data.market_data.current_price.usd;
-        const symbol = data.symbol;
-        valorMonedaElement.textContent = `Valor de la moneda: $${valorMoneda}`;
-        descripcionElement.textContent = symbol;
+        const valorMaximo = data.market_data.high_24h.usd;
+        const valorMinimo = data.market_data.low_24h.usd;
+        const cambio24h = data.market_data.price_change_24h;
+        const ultimaActualizacion = data.market_data.last_updated;
+
+        nombreElement.textContent = nombre;
+        valorMonedaElement.textContent = `Valor actual en USD: $${valorMoneda}`;
+        valorMaximoElement.textContent = `Valor máximo: $${valorMaximo}`;
+        valorMinimoElement.textContent = `Valor mínimoc: $${valorMinimo}`;
+        cambio24hElement.textContent = `Cambio en las últimas 24 horas: $${cambio24h}`;
+        ultimaActualizacionElement.textContent = `Última actualización: ${ultimaActualizacion}`;
+ 
         document.getElementById("btnfavorita").style.display = 'block';
-        
+        document.getElementById("hero") .style.display = 'none';
+        document.getElementById("resultado").style.display = 'block';
     })
     .catch(error => {
       console.log(error);
@@ -54,6 +66,7 @@ btnFavoritas.addEventListener('click', function () {
     const data = [selectedCoinId, valorMonedaElement.textContent, descripcionElement.textContent]
     const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
     const favoritosfull = JSON.parse(localStorage.getItem('favoritosfull')) || [];
+
     if (favoritos.includes(selectedCoinId)) {
         alert('Esta moneda ya está en la lista de favoritos.');
         return;
--- favoritas.js
+++ favoritas.js
@@ -1,15 +1,20 @@
-const favoritosContainer = document.getElementById('favoritas');
+const favoritosContainer = document.getElementById("favoritas");
 
+const nombreElement = document.getElementById('nombre');
+const valorMonedaElement = document.getElementById('valor');
 
 function mostrarFavoritos() {
-    favoritosContainer.innerHTML = '';
-    const favoritos = JSON.parse(localStorage.getItem('favoritosfull')) || [];
-    favoritos.forEach(favorito => {
-      const favoritoElement = document.createElement('div');
-      favoritoElement.textContent = favorito;
-  
-      favoritosContainer.appendChild(favoritoElement);
-    });
-  }
-  
-  mostrarFavoritos();
+  favoritosContainer.innerHTML = "";
+  const favoritos = JSON.parse(localStorage.getItem("favoritosfull")) || [];
+  favoritos.forEach((favorito) => {
+    const favoritoElement = document.createElement("div");
+    favoritoElement.classList.add("favorito");
+    favoritoElement.innerHTML = `
+      <div class="text-white">${favorito[0].toUpperCase()}</div>
+      <div class="text-white">${favorito[1]}</div>
+    `;
+    favoritosContainer.appendChild(favoritoElement);
+  });
+}
+
+mostrarFavoritos();
